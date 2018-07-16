6912a55a3492e2d32907769429baf858fec9157e
Israel Zurita <israel.5k@hotmail.com>
2018-07-16 02:00:51 -0500

    Creacion de barra de menu(ActionBar)

--- app/src/main/java/com/example/pasantias/appcamaleon/MapsActivity.java
+++ app/src/main/java/com/example/pasantias/appcamaleon/MapsActivity.java
@@ -5,6 +5,9 @@ import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -48,10 +51,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        Toolbar miActionBar = (Toolbar) findViewById(R.id.miActionBar);
+        //setSupportActionBar(miActionBar);
+
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //getActionBar(.setSupportActionBar(miActionBar);
+        ///getActionBar().setDisplayHomeAsUpEnabled(true);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
 
         //traerRutaDeClientes(clienteMins,tokenEjemplo);
 
--- app/src/main/java/com/example/pasantias/appcamaleon/MenuPrincipal.java
+++ app/src/main/java/com/example/pasantias/appcamaleon/MenuPrincipal.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -47,6 +50,9 @@ public class MenuPrincipal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
+        Toolbar miActionBar = (Toolbar)  findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);
+
         btVerPedidos = (ImageButton) findViewById(R.id.bt_ver_pedidos);
         btRutaClientes = (ImageButton) findViewById(R.id.bt_ruta_clientes);
         btListClientes = (ImageButton) findViewById(R.id.bt_list_clientes);
@@ -77,6 +83,21 @@ public class MenuPrincipal extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemThatWasSelected = item.getItemId();
+       if (menuItemThatWasSelected == R.id.action_test) {
+           Log.d("Menu", "item xxxx");
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @SuppressLint("LongLogTag")
